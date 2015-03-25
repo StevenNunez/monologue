fea085783374ae851d63350f5ce33db75fdb993d
Steven Nunez <steven.nunez@gmail.com>
2015-03-25 11:28:30 -0400

    Manually write  blocks

--- app/controllers/monologue/admin/posts_controller.rb
+++ app/controllers/monologue/admin/posts_controller.rb
@@ -1,13 +1,14 @@
 class Monologue::Admin::PostsController < Monologue::Admin::BaseController
-  respond_to :html
   before_filter :load_post, only: [:edit, :update]
-  
+
   def index
     @posts = Monologue::Post.default.includes(:user)
+    respond_to {|f| f.html}
   end
 
   def new
     @post = Monologue::Post.new
+    respond_to {|f| f.html}
   end
 
   ## Preview a post without saving.
@@ -17,40 +18,55 @@ class Monologue::Admin::PostsController < Monologue::Admin::BaseController
     @post.user_id = monologue_current_user.id
     @post.published_at = Time.zone.now
     # render it exactly as it would display when live.
-    render "/monologue/posts/show", layout: Monologue::Config.layout || "/layouts/monologue/application"
+    respond_to do |f| 
+      f.html { render "/monologue/posts/show", layout: Monologue::Config.layout || "/layouts/monologue/application" }
+    end
   end
 
   def create
-    @post = Monologue::Post.new post_params
-    @post.user_id = monologue_current_user.id
-    if @post.save
-      prepare_flash_and_redirect_to_edit()
-    else
-      render :new
+    respond_to do |f| 
+      f.html do
+        @post = Monologue::Post.new post_params
+        @post.user_id = monologue_current_user.id
+        if @post.save
+          prepare_flash_and_redirect_to_edit()
+        else
+          render :new
+        end
+      end
     end
   end
 
   def edit
+    respond_to {|f| f.html}
   end
 
   def update
-    if @post.update(post_params)
-      prepare_flash_and_redirect_to_edit()
-    else
-      render :edit
+    respond_to do |f| 
+      f.html do
+        if @post.update(post_params)
+          prepare_flash_and_redirect_to_edit()
+        else
+          render :edit
+        end
+      end
     end
   end
 
   def destroy
-    post = Monologue::Post.find(params[:id])
-    if post.destroy
-      redirect_to admin_posts_path, notice:  I18n.t("monologue.admin.posts.delete.removed")
-    else
-      redirect_to admin_posts_path, alert: I18n.t("monologue.admin.posts.delete.failed")
+    respond_to do |f| 
+      post = Monologue::Post.find(params[:id])
+      f.html do
+        if post.destroy
+          redirect_to admin_posts_path, notice:  I18n.t("monologue.admin.posts.delete.removed")
+        else
+          redirect_to admin_posts_path, alert: I18n.t("monologue.admin.posts.delete.failed")
+        end
+      end
     end
   end
 
-private
+  private
   def load_post
     @post = Monologue::Post.find(params[:id])
   end
